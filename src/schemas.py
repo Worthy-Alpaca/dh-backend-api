a4ad06e1dbbdeae082742d23e62fc71651878bcc
Worthy Alpaca <stephanxxschumacher@gmail.com>
2022-05-09 14:01:00 +0200

    added request data schemas

---
+++ src/schemas.py
@@ -0,0 +1,9 @@
+from pydantic import BaseModel
+from typing import Optional
+
+class DummyMachine(BaseModel):
+    machine: str
+    cph: int = 1
+    nozHeads: int =1
+    SMD: bool = False
+    offsets: Optional[dict] = None
