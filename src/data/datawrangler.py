77dba482aeb66cc2633f3dcfd2a205bc8d196860
Worthy Alpaca <stephanxxschumacher@gmail.com>
2022-05-12 16:01:00 +0200

    added logic to better calculate the time

--- src/data/datawrangler.py
+++ src/data/datawrangler.py
@@ -13,18 +13,51 @@ class DataWrangler:
         pathM20 = Path(os.getcwd() + os.path.normpath(f"/data/logs/{machine}/"))
         pd.options.mode.chained_assignment = None
 
+        product_DB = pd.read_csv(
+            os.getcwd() + os.path.normpath(f"/data/global/product_db_dummy.csv"),
+            encoding="unicode_escape",
+            sep=";",
+        )
+        product_DB_cols = ["Kopfmaterial", "Anzahl Komponenten in Kopfmaterial"]
+        product_DB = product_DB[product_DB_cols]
+        product_DB["Kopfmaterial"] = product_DB["Kopfmaterial"].apply(lambda x: x.replace(".", ""))
+        product_DB = product_DB.rename(columns={"Anzahl Komponenten in Kopfmaterial": "NumComp"})
+        product_DB = product_DB.drop_duplicates()
+        print(product_DB.info())
+
         def check(row):
+            if row == "LP begibt sich zu Punkt LP Einlaufband.":
+                return None
             found = re.search("(.+?) Updated PCB Count", row).group(1)
-            return found
+            return found[0:7]
+
+        def compare(x):
+            if x == None:
+                return 0
+            compareData = product_DB[product_DB["Kopfmaterial"].str.match(x)]
+            return compareData.NumComp.max()
+
+        temp = pd.DataFrame()
 
         for i in pathM20.iterdir():
             df = pd.read_csv(i, low_memory=False, encoding="unicode_escape")
             df = df.dropna()
             neededColumns = ["Date", "Contents"]
             df = df[neededColumns]
-            pcbcount = df.loc[df["Contents"].str.contains("Updated PCB Count", case=False)]
+            matchers = ["Updated PCB Count", "LP begibt sich zu Punkt LP Einlaufband."]
+            loc = "|".join(matchers)
+            pcbcount = df.loc[df["Contents"].str.contains(loc)]
+            print(pcbcount)
             pcbcount["Date"] = pd.to_datetime(pcbcount["Date"])
-            pcbcount["Product_Code"] = pcbcount["Contents"].apply(lambda x: check(x))
+            pcbcount["Kopfmaterial"] = pcbcount["Contents"].apply(lambda x: check(x))
+            pcbcount["NumComponents"] = pcbcount["Kopfmaterial"].apply(lambda x: compare(x))
+            """pcbcount = pd.merge(
+                left=pcbcount,
+                left_on="Kopfmaterial",
+                right=product_DB,
+                right_on="Kopfmaterial",
+                how="left",
+            )"""
 
             for index, row in pcbcount.iterrows():
                 nextIndex = index - 1
@@ -38,14 +71,15 @@ class DataWrangler:
                 timeNeeded = row.Date - nextLookUpTable.Date
                 timeNeeded = timeNeeded.total_seconds()
                 pcbcount.loc[index, "Time_Needed"] = timeNeeded
+            # pd.concat([temp, pcbcount])
 
-        outputpath = os.getcwd() + os.path.normpath("/data/logs/combined/data.csv")
-        """if exists(outputpath):
-            oldDF = pd.read_csv(outputpath, encoding='unicode_escape')
-            pd.concat([oldDF, pcbcount], ignore_index=True)
-            pcbcount = oldDF#.drop_duplicates()"""
-        print(len(pcbcount.index))
-        pcbcount.to_csv(outputpath, mode="a", header=not os.path.exists(outputpath))
+            outputpath = os.getcwd() + os.path.normpath(f"/data/logs/combined/data{machine}.csv")
+            """if exists(outputpath):
+                oldDF = pd.read_csv(outputpath, encoding='unicode_escape')
+                pd.concat([oldDF, pcbcount], ignore_index=True)
+                pcbcount = oldDF#.drop_duplicates()"""
+            pcbcount = pcbcount[pcbcount["Kopfmaterial"].notna()]
+            pcbcount.to_csv(outputpath, mode="a", header=not os.path.exists(outputpath))
 
     def returnData(self):
         df = pd.read_csv(
@@ -56,7 +90,7 @@ class DataWrangler:
 
 
 if __name__ == "__main__":
-    DataWrangler()
+    DataWrangler("m20")
     # df = pd.read_csv(os.getcwd() + os.path.normpath('/data/logs/combined/data.csv'), encoding='unicode_escape')
     # print(df.info())
     # print(df[['Date', 'Contents', 'Time_Needed']].drop_duplicates())
