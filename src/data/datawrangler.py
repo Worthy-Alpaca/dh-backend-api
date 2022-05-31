36dd6fd1745c2b6e1800405c52df28cd85ec4b59
Worthy Alpaca <stephanxxschumacher@gmail.com>
2022-05-31 16:00:04 +0200

    introduced MachineDataLoader into production

--- src/data/datawrangler.py
+++ src/data/datawrangler.py
@@ -1,7 +1,8 @@
 from pathlib import Path
 import pandas as pd
 import os
-from os.path import exists
+import concurrent.futures
+import time as tm
 import re
 
 
@@ -21,8 +22,12 @@ class DataWrangler:
         )
         product_DB_cols = ["Kopfmaterial", "Anzahl Komponenten in Kopfmaterial"]
         product_DB = product_DB[product_DB_cols]
-        product_DB["Kopfmaterial"] = product_DB["Kopfmaterial"].apply(lambda x: x.replace(".", ""))
-        product_DB = product_DB.rename(columns={"Anzahl Komponenten in Kopfmaterial": "NumComp"})
+        product_DB["Kopfmaterial"] = product_DB["Kopfmaterial"].apply(
+            lambda x: x.replace(".", "")
+        )
+        product_DB = product_DB.rename(
+            columns={"Anzahl Komponenten in Kopfmaterial": "NumComp"}
+        )
         product_DB = product_DB.drop_duplicates()
         print(product_DB.info())
 
@@ -51,7 +56,9 @@ class DataWrangler:
             print(pcbcount)
             pcbcount["Date"] = pd.to_datetime(pcbcount["Date"])
             pcbcount["Kopfmaterial"] = pcbcount["Contents"].apply(lambda x: check(x))
-            pcbcount["NumComponents"] = pcbcount["Kopfmaterial"].apply(lambda x: compare(x))
+            pcbcount["NumComponents"] = pcbcount["Kopfmaterial"].apply(
+                lambda x: compare(x)
+            )
             """pcbcount = pd.merge(
                 left=pcbcount,
                 left_on="Kopfmaterial",
@@ -77,81 +84,198 @@ class DataWrangler:
             self.pcbcount = pcbcount
 
     def returnData(self):
-        
+
         return self.pcbcount[["Date", "Contents", "Time_Needed"]].drop_duplicates()
 
     def saveData(self):
-        outputpath = os.getcwd() + os.path.normpath(f"/data/logs/combined/data{self.machine}.csv")
-        self.pcbcount.to_csv(outputpath, mode="a", header=not os.path.exists(outputpath))
+        outputpath = os.getcwd() + os.path.normpath(
+            f"/data/logs/combined/data{self.machine}.csv"
+        )
+        self.pcbcount.to_csv(
+            outputpath, mode="a", header=not os.path.exists(outputpath)
+        )
+
+
+class MyFancyException(Exception):
+    pass
 
 
 class MachineDataLoader:
-    def __init__(self) -> None:
-        path = Path(os.getcwd() + os.path.normpath(f"/data/logs/machine/"))
+    def __init__(self, machine) -> None:
+        path = Path(os.getcwd() + os.path.normpath(f"/data/logs/machine {machine}"))
 
-        concatDataframe = pd.DataFrame()
+        """try:
+            os.remove("output.txt")
+        except:
+            pass"""
+
+        timings = []
 
         for i in path.iterdir():
-
-            df = pd.read_csv(i, encoding="unicode_escape", skiprows=1)
-            df = df[["No.", "Date", "Component Code", "Fiducial#", "Offset"]]
+            start_time = tm.time()
+            print(f"Reading new Dataset: {i}", file=open("output.txt", "a"))
+            print(f"Reading new Dataset: {i}")
+            df = pd.read_csv(i, encoding="unicode_escape", skiprows=1, low_memory=False)
+            print(
+                "Dataset reading complete",
+                tm.time() - start_time,
+                file=open("output.txt", "a"),
+            )
+            print(
+                "Dataset reading complete",
+                tm.time() - start_time,
+            )
+            df = df[
+                ["No.", "Date", "Fiducial#", "Offset", "Head#", "Feed Style", "PCB ID"]
+            ]
             df = df.dropna()
             df = df.iloc[:-1, :]
-            concatDataframe = pd.concat([concatDataframe, df], ignore_index=True)
+            print(df.info())
+            # df["Fiducial#"] = df["Fiducial#"].astype(int)
+            # df["Offset"] = df["Offset"].astype(int)
+            df["No."] = df["No."].astype(int)
+            df["Date"] = pd.to_datetime(df["Date"], errors="coerce")
+            df["Head#"] = df["Head#"].astype(int)
+            # print(df["Fiducial#"].value_counts().drop_duplicate0s())
 
-        concatDataframe = concatDataframe[["No.", "Date", "Component Code", "Fiducial#", "Offset"]]
+            DFList = []
+
+            for n, g in df.groupby(by=df["Date"].dt.date):
+                DFList.append(g)
+
+            print(f"Reading logs for {len(DFList)} Days ", file=open("output.txt", "a"))
+            print(f"Reading logs for {len(DFList)} Days ")
+
+            output = pd.DataFrame()
+            dayCounter = 0
+            results = []
+            start = tm.perf_counter()
+            with concurrent.futures.ThreadPoolExecutor() as executor:
+                for day in DFList:
+                    dayCounter = dayCounter + 1
+                    print(f"Starting Day {dayCounter} ", file=open("output.txt", "a"))
+                    print(f"Starting Day {dayCounter} ")
+                    results.append(executor.submit(self.calcTimings, day, dayCounter))
+
+            for f in results:
+                dayoutput = f.result()
+                output = pd.concat([output, dayoutput], ignore_index=True)
+            print(
+                f"===== Duration Multithreading: {tm.perf_counter() - start} =====",
+                file=open("output.txt", "a"),
+            )
+            print(f"===== Duration Multithreading: {tm.perf_counter() - start} =====")
+            output = output.drop_duplicates()
+            output.sort_values(by="StartTime", inplace=True)
+            # endtime = output["EndTime"].max()
+            # starttime = output["StartTime"].min()
+            # endtime = re.search("(.+?) (.+?)", str(endtime)).group(1)
+            # starttime = re.search("(.+?) (.+?)", str(starttime)).group(1)
+            outputPath = Path(
+                os.getcwd() + os.path.normpath(f"/data/logs/combined/m20_timings.csv")
+            )
+            output.to_csv(outputPath, mode="a", header=not os.path.exists(outputPath))
+            """start = tm.perf_counter()
+            dayCounter = 0
+            for day in DFList:
+                dayCounter = dayCounter + 1
+                print(f"Starting sequential Day {dayCounter} ")
+                self.calcTimings(day, dayCounter)
+
+            print(f"===== Duration Sequential: {tm.perf_counter() - start} =====")"""
+
+    def calcTimings(self, day: pd.DataFrame, dayCount: int) -> pd.DataFrame:
         list_df = []
-
-        concatDataframe["Date"] = pd.to_datetime(concatDataframe["Date"], errors="coerce")
-        concatDataframe["Offset"] = concatDataframe["Offset"].astype(int)
-
-        # concatDataframe = concatDataframe.sort_values(by="Date", inplace=True)
-        concatDataframe.to_csv(os.getcwd() + "test.csv")
-        for n, g in concatDataframe.groupby("Fiducial#"):
-            print(g)
+        for n, g in day.groupby(["PCB ID"]):
+            # print(g, file=open("output.txt", "a"))
             list_df.append(g)
-
-        prev = []
-        nxt = []
-        # print(list_df)
-        # return
+        print(
+            f"Processing {len(list_df)} Batches for Day {dayCount}",
+            file=open("output.txt", "a"),
+        )
+        print(f"Processing {len(list_df)} Batches for Day {dayCount}")
+        placementTimings = []
         for i, v in enumerate(list_df):
-            if i + 1 >= len(list_df):
-                break
-            nextV = list_df[i + 1]
-            if nextV["Offset"].max() == 0:
-                prev.append({"time": v["Date"].min(), "index": v["Date"].index[0]})
-                prev.append({"time": nextV["Date"].max(), "index": nextV["Date"].index[0]})
 
-        prev.pop()
-        prev.pop(0)
+            start = v["Date"].min()
+            end = v["Date"].max()
+            placementStart = v["No."].min()
+            placementEnd = v["No."].max()
+            placementTimings.append(
+                {
+                    "start": start,
+                    "placement": placementStart,
+                    "head": v["Head#"],
+                    "FeedStyle": v["Feed Style"],
+                }
+            )
+            placementTimings.append(
+                {
+                    "end": end,
+                    "placement": placementEnd,
+                    "head": v["Head#"],
+                    "FeedStyle": v["Feed Style"],
+                }
+            )
+            # timings = timings + placementTimings
+
+            if "end" in list(placementTimings[0].keys()):
+                placementTimings.pop(0)
+            # print(placementTimings)
 
         def pairwise(iterable):
-            "s -> (s0, s1), (s2, s3), (s4, s5), ..."
             a = iter(iterable)
             return zip(a, a)
 
-        output = pd.DataFrame()
+        if "end" in list(placementTimings[0].keys()):
+            placementTimings.pop(0)
+        del list_df
+        print(f"Calculating Timings for Day {dayCount}", file=open("output.txt", "a"))
+        print(f"Calculating Timings for Day {dayCount}")
+        while True:
+            try:
+                # print("Org. List", len(placementTimings))
+                # print("Output List", len(output))
+                outputDay = pd.DataFrame(None)
+                for x, y in pairwise(placementTimings):
+                    try:
+                        timeNeeded = y["end"] - x["start"]
+                        placementsNeeded = y["placement"] - x["placement"]
+                        timeNeeded = (
+                            1
+                            if timeNeeded.total_seconds() == 0
+                            else timeNeeded.total_seconds()
+                        )
+                        heads = day["Head#"].max()
+                        if heads == 4:
+                            machine = "m20"
+                        else:
+                            machine = "m10"
+                        dataDict = {
+                            "StartTime": x["start"],
+                            "EndTime": y["end"],
+                            "placementsNeeded": placementsNeeded,
+                            "timeNeeded": timeNeeded,
+                            "machine": machine,
+                            "heads": heads,
+                            "cph": (placementsNeeded / timeNeeded) * 3600,
+                        }
 
-        for x, y in pairwise(prev):
-            timeNeeded = y["time"] - x["time"]
-            placementsNeeded = y["index"] - x["index"]
-            dataDict = {
-                "StartTime": x["time"],
-                "EndTime": y["time"],
-                "placementsNeeded": placementsNeeded,
-                "timeNeeded": timeNeeded.total_seconds(),
-            }
+                        dataDict = pd.DataFrame(dataDict, [0])
 
-            dataDict = pd.DataFrame(dataDict, [0])
-
-            output = pd.concat([output, dataDict], ignore_index=True)
-
-        self.data = output
-        print(output)
-
-    def returnData(self) -> pd.DataFrame:
-        return self.data.to_dict()
+                        outputDay = pd.concat([outputDay, dataDict], ignore_index=True)
+                        outputDay.sort_values(by="StartTime", inplace=True)
+                    except:
+                        # print("An error occured. Removing false Data")
+                        placementTimings.remove(x)
+                        raise MyFancyException
+                break
+            except:
+                continue
+        print(f"Calculations for Day {dayCount} complete", file=open("output.txt", "a"))
+        print(f"Calculations for Day {dayCount} complete")
+        del placementTimings
+        return outputDay
 
 
 if __name__ == "__main__":
